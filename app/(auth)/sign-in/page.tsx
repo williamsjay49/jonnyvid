ff247aa90b8c8422263d1da70331437385cb2271
johnson williams <williams.gb55@gmail.com>
2025-05-29 17:55:12 +0900

    auth-ui-complete

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,79 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className="sign-in">
+      <aside className="testimonial">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={50}
+            height={30}
+          />
+        </Link>
+
+        <div className="description">
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Image
+                  alt="star"
+                  src="/assets/icons/star.svg"
+                  width={20}
+                  height={20}
+                  key={index}
+                />
+              ))}
+            </figure>
+            <p>
+              Oreon makes screen recording easy. From quick walkthrough to full
+              presentation, it's fast, smooth, and shareable in seconds
+            </p>
+            <article>
+              <Image
+                alt="dummy"
+                src="/assets/images/dummy.jpg"
+                height={64}
+                width={64}
+                className="rounded-full"
+              />
+              <div>
+                <h2>John Doe</h2>
+                <p>Product Designer at Kampy</p>
+              </div>
+            </article>
+          </section>
+        </div>
+        <p>© Oreon {new Date().getFullYear()}</p>
+      </aside>
+      <aside className="google-sign-in">
+        <section>
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="logo"
+              width={90}
+              height={30}
+            />
+          </Link>
+          <p>Create your very first story with Oren</p>
+          <button>
+            <Image
+              alt="google"
+              src="/assets/icons/google.svg"
+              width={22}
+              height={22}
+            />
+            <span>Sign in with Google</span>
+          </button>
+        </section>
+      </aside>
+      <div className="overlay" />
+    </main>
+  );
 };
 
 export default page;
